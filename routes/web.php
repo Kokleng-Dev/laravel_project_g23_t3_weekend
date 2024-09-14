85761f21f8bc20b5b0fd5bb34f2cd96fb8919c23
KOKLENG <kokleng@dev.local>
2024-09-14 12:15:11 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,22 @@ Route::get('information/{data}/detail/{id}', function($data,$id){
     echo "<br>";
     echo "<a href='". route('home') ."'>Home</a>";
 })->name('info');
+
+
+
+Route::get('/form', function(){
+    return view('form');
+})->name('form');
+
+Route::post('/submit-form/{id}',function($id){
+    // echo 'hi ' . $id . "<br>";
+
+    // echo request()->get('myinput');
+ 
+})->name('form.submit');
+
+
+Route::fallback(function(){
+    
+    return redirect()->route('info', [1,2]);
+});
